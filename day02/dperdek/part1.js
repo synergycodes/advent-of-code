c62aa70bb1217c5fd17b32ce9c21e6e1bb268b92
Dawid Perdek <perdekdawid@gmail.com>
2021-12-02 09:32:14 +0100

    [dp] day02 both parts solved

---
+++ day02/dperdek/part1.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const readline = require('readline');
+
+(() => {
+    const lineReader = readline.createInterface({
+        input: fs.createReadStream('data.txt')
+    });
+
+    let position = 0;
+    let depth = 0;
+
+    lineReader.on('line', line => {
+        const [command, valueAsText] = line.split(' ');
+        const value = Number(valueAsText);
+
+        switch (command) {
+            case 'forward': position += value; break;
+            case 'up': depth -= value; break;
+            case 'down': depth += value; break;
+            default: console.log('invalid command');
+        }
+    });
+
+    lineReader.on('close', () => {
+        console.log(`position: ${position} * depth: ${depth} = ${position * depth}`);
+    });
+})();
