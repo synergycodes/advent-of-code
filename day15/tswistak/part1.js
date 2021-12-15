3098751f25a34b03fd72a80d7aac02da1a90c346
Tomasz Świstak <swistak.tomasz@outlook.com>
2021-12-15 19:11:52 +0100

    [ts] day15 solved

---
+++ day15/tswistak/part1.js
@@ -0,0 +1,103 @@
+const { once } = require("events");
+const fs = require("fs");
+const readline = require("readline");
+
+(async () => {
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream(__dirname + "/data.txt"),
+  });
+
+  const map = [];
+
+  lineReader.on("line", (line) => {
+    map.push([...line].map((x) => parseInt(x)));
+  });
+
+  await once(lineReader, "close");
+
+  const startI = 0;
+  const startJ = 0;
+  const endI = map.length - 1;
+  const endJ = map[endI].length - 1;
+
+  const getKey = (i, j) => `${i},${j}`;
+  const getCoords = (key) => key.split(",").map((x) => parseInt(x));
+
+  const queue = [];
+  const distances = new Map();
+  const previous = new Map();
+
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      const key = getKey(i, j);
+      queue.push(key);
+      distances.set(key, Number.POSITIVE_INFINITY);
+      previous.set(key, null);
+    }
+  }
+  distances.set(getKey(startI, startJ), 0);
+
+  const findMinVertexIndex = () => {
+    let minValue = Number.POSITIVE_INFINITY;
+    let minVertexIndex = 0;
+    queue.forEach((vertex, i) => {
+      const distance = distances.get(vertex);
+      if (distance < minValue) {
+        minValue = distance;
+        minVertexIndex = i;
+      }
+    });
+    return minVertexIndex;
+  };
+
+  const getNeighbors = (vertex) => {
+    const [i, j] = getCoords(vertex);
+    const result = [];
+    if (i > 0) {
+      const up = getKey(i - 1, j);
+      if (queue.indexOf(up) > -1) {
+        result.push(up);
+      }
+    }
+    if (i < endI) {
+      const down = getKey(i + 1, j);
+      if (queue.indexOf(down) > -1) {
+        result.push(down);
+      }
+    }
+    if (j > 0) {
+      const left = getKey(i, j - 1);
+      if (queue.indexOf(left) > -1) {
+        result.push(left);
+      }
+    }
+    if (j < endJ) {
+      const right = getKey(i, j + 1);
+      if (queue.indexOf(right) > -1) {
+        result.push(right);
+      }
+    }
+    return result;
+  };
+
+  const endKey = getKey(endI, endJ);
+
+  while (queue.length > 0) {
+    console.log("queue length", queue.length);
+    const currentIndex = findMinVertexIndex();
+    const current = queue[currentIndex];
+    queue.splice(currentIndex, 1);
+    const neighbors = getNeighbors(current);
+    for (const vertex of neighbors) {
+      const [nI, nJ] = getCoords(vertex);
+      const newDistance = distances.get(current) + map[nI][nJ];
+      if (newDistance < distances.get(vertex)) {
+        distances.set(vertex, newDistance);
+        previous.set(vertex, current);
+      }
+    }
+  }
+
+  const length = distances.get(endKey);
+  console.log("length", length);
+})();
