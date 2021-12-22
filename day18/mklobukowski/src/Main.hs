4c18c058b2d5c82923acb79af6e2c8b87a28055d
burzomir <burzomirdev@gmail.com>
2021-12-22 14:57:52 +0100

    Implement explode

--- day18/mklobukowski/src/Main.hs
+++ day18/mklobukowski/src/Main.hs
@@ -79,8 +79,8 @@ previous l@(RegularNumber _, Left' _ _) =
       goUp l'@(Pair _ _, Top) = Nothing
       goUp _ = Nothing
       goDown l'@(RegularNumber _, _) = l'
-      goDown l' = goDown . right $  l'
-   in  fmap goDown . goUp . up $ l
+      goDown l' = goDown . right $ l'
+   in fmap goDown . goUp . up $ l
 previous _ = Nothing
 
 next :: Location -> Maybe Location
@@ -91,23 +91,28 @@ next l@(RegularNumber _, Right' _ _) =
       goUp l'@(Pair _ _, Top) = Nothing
       goUp _ = Nothing
       goDown l'@(RegularNumber _, _) = l'
-      goDown l' = goDown . left $  l'
-   in  fmap goDown . goUp . up $ l
+      goDown l' = goDown . left $ l'
+   in fmap goDown . goUp . up $ l
 next _ = Nothing
+
+explode :: (SnailfishNumber, Context) -> Maybe Location
+explode l@(Pair (RegularNumber nl) (RegularNumber nr), _) =
+  let s1 = left l
+      s2 = case previous s1 of
+        Just (RegularNumber n, c) -> next (RegularNumber (n + nl), c)
+        _ -> Just s1
+      s3 = case s2 >>= next >>= next of
+        Just (RegularNumber n, c) -> previous (RegularNumber (n + nr), c)
+        _ -> s2
+   in case up <$> s3 of
+        Just (Pair _ _, c) -> Just (RegularNumber 0, c)
+        _ -> Nothing
+explode l = Nothing
 
 main :: IO ()
 main = do
-  print . right . right . top $ sample3
+  print . fmap upMost . explode . left . top $ sample3
   print ""
-  print . previous . right . right . top $ sample3
-  print ""
-  print .  ((=<<) previous . previous) . right . right . top $ sample3
-  print ""
-  print .  ((=<<) previous . (=<<) previous . previous) . right . right . top $ sample3
-  print ""
-  print .  ((=<<) next . (=<<) previous . (=<<) previous . previous) . right . right . top $ sample3
-  print ""
-  print .  ((=<<) next . (=<<) next . (=<<) previous . (=<<) previous . previous) . right . right . top $ sample3
 
 -- print . previous . right . right . left . left . left . top $ sample1
 -- print . previous . right . left . left . left . left . top $ sample
