9688732fb3a64e1ec7cccbc7275c027d814fd7d1
burzomir <burzomirdev@gmail.com>
2021-12-26 20:38:57 +0100

    Reimplement previous

--- day18/mklobukowski/src/Main.hs
+++ day18/mklobukowski/src/Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
 import Data.Foldable
+import Control.Monad ( (<=<) )
 
 data SnailfishNumber
   = RegularNumber Int
   | Pair SnailfishNumber SnailfishNumber
-  deriving (Show)
+  deriving (Show, Eq)
 
 type Level = Int
 
@@ -13,33 +14,81 @@ data Context
   = Top
   | Left' Context SnailfishNumber
   | Right' Context SnailfishNumber
-  deriving (Show)
+  deriving (Show, Eq)
 
-type Location = (SnailfishNumber, Context)
+type Location = (SnailfishNumber, Context, Level)
 
 top :: SnailfishNumber -> Location
-top n = (n, Top)
+top n = (n, Top, 1)
 
 left :: Location -> Location
-left rn@(RegularNumber n, _) = rn
-left (Pair n1 n2, c) = (n1, Left' c n2)
+left rn@(RegularNumber n, _, _) = rn
+left (Pair n1 n2, c, l) = (n1, Left' c n2, l + 1)
 
 right :: Location -> Location
-right rn@(RegularNumber n, _) = rn
-right (Pair n1 n2, c) = (n2, Right' c n1)
+right rn@(RegularNumber n, _, _) = rn
+right (Pair n1 n2, c, l) = (n2, Right' c n1, l + 1)
 
 up :: Location -> Location
-up l@(_, Top) = l
-up (n, Left' c n') = (Pair n n', c)
-up (n, Right' c n') = (Pair n' n, c)
+up l@(_, Top, _) = l
+up (n, Left' c n', l) = (Pair n n', c, l - 1)
+up (n, Right' c n', l) = (Pair n' n, c, l - 1)
 
 upMost :: Location -> SnailfishNumber
-upMost (n, Top) = n
+upMost (n, Top, _) = n
 upMost l = upMost . up $ l
 
 p = Pair
 
 n = RegularNumber
+
+-- previous :: Location -> Maybe Location
+-- previous l@(RegularNumber _, Right' _ _, _) = Just . left . up $ l
+-- previous l@(RegularNumber _, Left' _ _, _) =
+--   let goUp l'@(Pair _ _, Right' _ _, _) = Just . left . up $ l'
+--       goUp l'@(Pair _ _, Left' _ _, _) = goUp . up $ l'
+--       goUp l'@(Pair _ _, Top, _) = Nothing
+--       goUp _ = Nothing
+--       goDown l' =
+--         let l'' = right l'
+--          in if l'' == l' then l' else goDown l''
+--    in fmap goDown . goUp . up $ l
+-- previous _ = Nothing
+
+-- next :: Location -> Maybe Location
+-- next l@(RegularNumber _, Left' _ _, _) = Just . right . up $ l
+-- next l@(RegularNumber _, Right' _ _, _) =
+--   let goUp l'@(Pair _ _, Left' _ _, _) = Just . right . up $ l'
+--       goUp l'@(Pair _ _, Right' _ _, _) = goUp . up $ l'
+--       goUp l'@(Pair _ _, Top, _) = Nothing
+--       goUp _ = Nothing
+--       goDown l'@(RegularNumber _, _, _) = l'
+--       goDown l' = goDown . left $ l'
+--    in fmap goDown . goUp . up $ l
+-- next _ = Nothing
+
+-- explode :: Location -> Maybe Location
+-- explode l@(Pair (RegularNumber nl) (RegularNumber nr), _, _) =
+--   let s1 = left l
+--       s2 = case previous s1 of
+--         Just (RegularNumber n, c, l) -> next (RegularNumber (n + nl), c, l)
+--         _ -> Just s1
+--       s3 = case s2 >>= next >>= next of
+--         Just (RegularNumber n, c, l) -> previous (RegularNumber (n + nr), c, l)
+--         _ -> s2
+--    in case up <$> s3 of
+--         Just (Pair _ _, c, l) -> Just (RegularNumber 0, c, l)
+--         _ -> Nothing
+-- explode l = Nothing
+
+previous l@(_, Top, _) = Nothing
+previous l@(_, Left' _ _, _) = Just . up $ l
+previous l@(_, Right' _ _, _) =
+  let goUp l'@(_, Right' _ _, _) = Just . left . up $ l'
+      goUp l'@(_, Left' _ _, _) = goUp . up $ l'
+      goUp l'@(_, Top, _) = Nothing
+      goRight l' = if l' == right l' then l' else goRight . right $ l'
+   in fmap goRight . goUp $ l
 
 -- [[[[[9,8],1],2],3],4]
 sample1 =
@@ -52,45 +101,11 @@ sample2 =
 sample3 =
   p (p (n 1) (n 2)) (p (n 3) (n 4))
 
-previous :: Location -> Maybe Location
-previous l@(RegularNumber _, Right' _ _) = Just . left . up $ l
-previous l@(RegularNumber _, Left' _ _) =
-  let goUp l'@(Pair _ _, Right' _ _) = Just . left . up $ l'
-      goUp l'@(Pair _ _, Left' _ _) = goUp . up $ l'
-      goUp l'@(Pair _ _, Top) = Nothing
-      goUp _ = Nothing
-      goDown l'@(RegularNumber _, _) = l'
-      goDown l' = goDown . right $ l'
-   in fmap goDown . goUp . up $ l
-previous _ = Nothing
-
-next :: Location -> Maybe Location
-next l@(RegularNumber _, Left' _ _) = Just . right . up $ l
-next l@(RegularNumber _, Right' _ _) =
-  let goUp l'@(Pair _ _, Left' _ _) = Just . right . up $ l'
-      goUp l'@(Pair _ _, Right' _ _) = goUp . up $ l'
-      goUp l'@(Pair _ _, Top) = Nothing
-      goUp _ = Nothing
-      goDown l'@(RegularNumber _, _) = l'
-      goDown l' = goDown . left $ l'
-   in fmap goDown . goUp . up $ l
-next _ = Nothing
-
-explode :: (SnailfishNumber, Context) -> Maybe Location
-explode l@(Pair (RegularNumber nl) (RegularNumber nr), _) =
-  let s1 = left l
-      s2 = case previous s1 of
-        Just (RegularNumber n, c) -> next (RegularNumber (n + nl), c)
-        _ -> Just s1
-      s3 = case s2 >>= next >>= next of
-        Just (RegularNumber n, c) -> previous (RegularNumber (n + nr), c)
-        _ -> s2
-   in case up <$> s3 of
-        Just (Pair _ _, c) -> Just (RegularNumber 0, c)
-        _ -> Nothing
-explode l = Nothing
+sample4 =
+  -- [[1,2], 3]
+  p (p (n 1) (n 2)) (n 3)
 
 main :: IO ()
 main = do
-  print . fmap upMost . explode . left . top $ sample3
+  print . previous . right . top $ sample4
   print ""
