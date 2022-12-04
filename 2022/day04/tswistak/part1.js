bc6f1a4cdf655e6ef4a0165eda98fccb0e2453a8
Tomasz Świstak <tomasz.swistak@synergycodes.com>
2022-12-04 11:08:52 +0100

    [ts] day 4 solved

---
+++ 2022/day04/tswistak/part1.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(__dirname + "/input.txt", { encoding: "utf-8" });
+
+let result = 0;
+
+for (const line of input.split("\n")) {
+  const [begin1, end1, begin2, end2] = line
+    .split(/,|-/)
+    .map((x) => parseInt(x));
+  if (
+    (begin1 >= begin2 && end1 <= end2) ||
+    (begin2 >= begin1 && end2 <= end1)
+  ) {
+    result++;
+  }
+}
+
+console.log(result);
