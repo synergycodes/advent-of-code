beb82a0ceb096fce8286e7dd4451b031054eafd6
Dawid Perdek <perdekdawid@gmail.com>
2022-12-05 18:46:26 +0100

    [dp] day 4 solved

---
+++ 2022/day04/dperdek/part1.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(`${__dirname}/input.txt`, { encoding: 'utf-8' });
+
+let result = 0;
+
+for (const line of input.split('\n')) {
+    const [start1, end1, start2, end2] = line
+        .split(/,|-/)
+        .map((x) => parseInt(x));
+    if (
+        start1 >= start2 && end1 <= end2 ||
+        start2 >= start1 && end2 <= end1
+    ) {
+        result++;
+    }
+}
+
+console.log(result);
